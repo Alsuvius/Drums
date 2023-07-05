30c4ab0f807f6f9e8362530c128f1ea3059c14c9
Alex Price <a-r-price@hotmail.co.uk>
2023-07-05 09:53:53 +0100

    working button animations

---
+++ index.js
@@ -0,0 +1,57 @@
+document.addEventListener("keydown", function(event){
+        // makeSound(event.key);
+        buttonAnimation(event.keyCode);
+        console.log(event.keyCode);
+})
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector(`.key[data-key="${currentKey}"]`);
+    activeButton.classList.add("playing");
+    setTimeout(() => {
+        activeButton.classList.remove("playing")
+    }, 100);
+}
+
+
+function makeSound(key){
+    switch (key) {
+        case 'w':
+            let tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case 'a':
+            let tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case 's':
+            let tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case 'd':
+            let tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case 'j':
+            let snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case 'k':
+            let kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        case 'l':
+            let crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        default:
+            console.log(buttonInnerHTML);
+            break;
+}}
+
