411623db3d630a0652bd421c734e1c59b8185ad5
Alex Price <a-r-price@hotmail.co.uk>
2023-07-05 10:33:06 +0100

    final final

--- index.js
+++ index.js
@@ -14,54 +14,7 @@ function buttonAnimation(currentKey){
 
 
 function makeSound(key){
-    switch (key) {
-        case 65:
-            let clap = new Audio('sounds/clap.wav');
-            clap.play();
-            break;
-
-        case 83:
-            let hihat = new Audio('sounds/hihat.wav');
-            hihat.play();
-            break;
-
-        case 68:
-            let kick = new Audio('sounds/kick.wav');
-            kick.play();
-            break;
-
-        case 70:
-            let openhat = new Audio('sounds/openhat.wav');
-            openhat.play();
-            break;
-
-        case 71:
-            let boom = new Audio('sounds/boom.wav');
-            boom.play();
-            break;
-
-        case 72:
-            let ride = new Audio('sounds/ride.wav');
-            ride.play();
-            break;
-
-        case 74:
-            let snare = new Audio('sounds/snare.wav');
-            snare.play();
-            break;
-
-        case 75:
-            let tom = new Audio('sounds/tom.wav');
-            tom.play();
-            break;
-
-        case 76:
-            let tink = new Audio('sounds/tink.wav');
-            tink.play();
-            break;
-
-        default:
-            console.log(event.keyCode);
-            break;
-}}
+    let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    audio.play();
+}
 
